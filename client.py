551a20ac554a93b4511b0d890813cf694d240c82
mihiro <f.mihiroxx@gmail.com>
2019-02-22 19:38:22 +0900

    change ip

--- client.py
+++ client.py
@@ -2,6 +2,6 @@ import socket
 
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # ここは環境に合わせた接続先を入れる
-my_socket.connect(('10.1.1.27', 50030))
+my_socket.connect(('192.168.10.104', 50030))
 my_text = "Hello! This is test message from my sample client!"
 my_socket.sendall(my_text.encode('utf-8'))
