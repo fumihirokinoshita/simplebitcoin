4d46132a50c2d5dfc79c2ecf50229ab8a7160092
mihiro <f.mihiroxx@gmail.com>
2019-03-04 15:02:08 +0900

    check def
    SampleClient && SampleServer1 && SampleServer2

--- SampleClient.py
+++ SampleClient.py
@@ -5,13 +5,16 @@ from core.client_core import ClientCore
 my_p2p_client = None
 
 def signal_handler(signal, frame):
+    print('sc1 signal_handler')
     shutdown_client()
 
 def shutdown_client():
+    print('sc1 shutdown_client')
     global my_p2p_client
     my_p2p_client.shutdown()
 
 def main():
+    print('sc1 main')
     signal.signal(signal.SIGINT, signal_handler)
     global my_p2p_client
     my_p2p_client = ClientCore(50095, '192.168.10.104', 50082)
--- SampleServer1.py
+++ SampleServer1.py
@@ -7,15 +7,18 @@ my_p2p_server = None
 
 
 def signal_handler(signal, frame):
+    print('ss1 signal_handler')
     shutdown_server()
 
 
 def shutdown_server():
+    print('ss1 shutdown_server')
     global my_p2p_server
     my_p2p_server.shutdown()
 
 
 def main():
+    print('ss1 main')
     signal.signal(signal.SIGINT, signal_handler)
     global my_p2p_server
     # 始原のCoreノードとして起動する
