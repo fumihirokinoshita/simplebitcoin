64fa13a3eecf681a9369daa05b56a213ebf68fd3
mihiro <f.mihiroxx@gmail.com>
2019-03-08 01:31:19 +0900

    add consensus system (wip : work in progress)
    Don't running the program

--- blockchain/blockchain_manager.py
+++ blockchain/blockchain_manager.py
@@ -1,6 +1,8 @@
 import json
 import hashlib
 import binascii
+import pickle
+import copy
 import threading
 
 class BlockchainManager:
@@ -18,19 +20,132 @@ class BlockchainManager:
         with self.lock:
             self.chain.append(block)
 
-    def is_valid_chain(self, chain):
+    def renew_my_blockchain(self, blockchian):
+        # ブロックチェーン自体を更新し、それによって変更されるはずの最新のprev_block_hashを計算して返却する
+        with self.lock:
+            if self.is_valid_chain(blockchian):
+                self.chain = blockchian
+                latest_block = self.chain[-1]
+                return self.get_hash(latest_block)
+            else:
+                print('invalid chain cannot be set...')
+                return None
 
+    def get_my_blockchain(self):
+        if len(self.chain) > 1:
+            return self.chain
+        else:
+            return None
+
+    def get_transactions_from_orphan_blocks(self, orphan_blocks):
+        current_index = 1
+        new_transactions = 0
+
+        while current_index < len(orphan_blocks):
+            block = orphan_blocks[current_index]
+            transactions = block['transactions']
+            target = self.remove_useless_transaction(transactions)
+            for t in target:
+                new_transactions.append(t)
+
+        return new_transactions
+
+    def remove_useless_transaction(self, transaction_pool):
+        """
+        与えられた Transactionのリストの中ですでに自分が管理する
+        ブロックチェーン内に含まれたTransactionがある場合、
+        それを削除したものを返却する
+        """
+
+        if len(transaction_pool) != 0:
+            current_index = 1
+            while current_index < len(self.chain):
+                block = self.chain[current_index]
+                transactions = block['transactions']
+                for t in transactions:
+                    for t2 in transaction_pool:
+                        # ブロックに格納するタイミングで json.dumps
+                        # してるので普通に比較すると死ぬ
+                        if t == json.dumps(t2):
+                            print('already exist in my blockchain :', t2)
+                            transaction_pool.remove(t2)
+
+                current_index += 1
+            return transaction_pool
+        else:
+            print('no transaction to be removed...')
+            return []
+
+    def resolve_conflicts(self, chain):
+        # 自分のブロックチェーンと比較して、長い方を有効とする。
+        # 有効性検証自体はrenew_my_blockchainで実施
+        mychain_len = len(self.chain)
+        new_chain_len = len(chain)
+
+        pool_4_orphan_blocks = copy.deepcopy(self.chain)
+        has_orphan = False
+
+        # 自分のチェーンの中でだけ処理済みになっているTransactionを救出する。
+        # 現在のチェーンに含まれていないブロックを全て取り出す。
+        # 時系列を考えての有効無効判定などはしないかなり簡易な処理。
+        if new_chain_len > mychain_len:
+            for b in pool_4_orphan_blocks:
+                for b2 in chain:
+                    if b == b2:
+                        pool_4_orphan_blocks.remove(b)
+
+            result = self.renew_my_blockchain(chain)
+            print(result)
+            if result is not None:
+                return result, pool_4_orphan_blocks
+            else:
+                return None, []
+        else:
+            print('invalid chain cannot be set...')
+            return None, []
+
+    def is_valid_block(self, prev_block_hash, block, difficulty=3):
+        # ブロック単体の正当性を検証する
+        suffix = '0' * difficulty
+        block_4_pow = copy.deepcopy(block)
+        nonce = block_4_pow['nonce']
+        del block_4_pow['nonce']
+        print(block_4_pow)
+
+        message = json.dumps(block_4_pow, sort_keys=True)
+        # print("message", message)
+        nonce = str(nonce)
+
+        if block['previous_block'] != prev_block_hash:
+            print('Invalid block (bad previous_block)')
+            print(block['previous_block'])
+            print(prev_block_hash)
+            return False
+        else:
+            digest = binascii.hexlify(self._get_double_sha256((message + nonce).encode('utf-8'))).decode('ascii')
+            if digest.endswith(suffix):
+                print('OK, this seems valid block')
+                return
+            else:
+                print('Invalid block (bad nonce)')
+                print('nonce :', nonce)
+                print('digest :', digest)
+                print('suffix :', suffix)
+                return False
+
+    def is_valid_chain(self, chain):
+        # ブロック全体の正当性を検証する
         last_block = chain[0]
         current_index = 1
 
         while current_index < len(chain):
-            block = self.chain[current_index]
-            if block['previous_block'] != self.get_hash(last_block):
+            block = chain[current_index]
+            if self.is_valid_block(self.get_hash(last_block), block) is not True:
                 return False
-
-            last_block = block
+            
+            last_block = chain[current_index]
             current_index += 1
-        
+
         return True
 
     def _get_double_sha256(self, message):
--- core/server_core.py
+++ core/server_core.py
@@ -1,5 +1,6 @@
 import time
 import socket, threading, json
+import pickle
 
 from blockchain.blockchain_manager import BlockchainManager
 from blockchain.block_builder import BlockBuilder
@@ -65,34 +66,49 @@ class ServerCore(object):
     def get_my_current_state(self):
         return self.server_state
 
+    def get_all_chains_for_resolve_conflict(self):
+        print('get_all_chains_for_resolve_conflict called')
+        new_message = self.cm.get_message_text(MSG_REQUEST_FULL_CHAIN)
+        self.cm.send_msg_to_all_peer(new_message)
+
     def __generate_block_with_tp(self):
 
-        result = self.tp.get_stored_transactions()
-        print('generate_block_with_tp called!')
-        if len(result) == 0:
-            print('Transaciton Pool is empty ...')
-        new_block = self.bb.generate_new_block(result, self.prev_block_hash)
-        self.bm.set_new_block(new_block.to_dict())
-        self.prev_block_hash = self.bm.get_hash(new_block.to_dict())
-        # ここから
-        message_new_block = self.cm.get_message_text(MSG_NEW_BLOCK, new_block.to_dict())
-        self.cm.send_msg_to_all_peer(message_new_block)
-        # ここまで追加
-        index = len(result)
-        self.tp.clear_my_transactions(index)
+        print('Thread for generate_block_with_tp started!')
+        while self.flag_stop_block_build is not True:
+
+            result = self.tp.get_stored_transactions()
+
+            if result == None:
+                print('Transacton Pool is empty ...')
+                break
+            new_tp = self.bm.remove_useless_transaction(result)
+            self.tp.renew_my_transaction(new_tp)
+            if len(result) == 0:
+                break
+            new_block = self.bb.generate_new_block(new_tp, self.prev_block_hash)
+            self.bm.set_new_block(new_block.to_dict())
+            self.prev_block_hash = self.bm.get_hash(new_block.to_dict())
+            message_new_block = self.cm.get_message_text(MSG_NEW_BLOCK, json.dumps(new_block.to_dict()))
+            self.cm.send_msg_to_all_peer(message_new_block)
+            index = len(result)
+            self.tp.clear_my_transactions(index)
+            break
 
         print('Current Blockchain is ... ', self.bm.chain)
         print('Current prev_block_hash is ... ', self.prev_block_hash)
-
+        self.flag_stop_block_build = False
+        self.is_bb_running = False
         self.bb_timer = threading.Timer(CHECK_INTERVAL, self.__generate_block_with_tp)
         self.bb_timer.start()
 
     def __handle_message(self, msg, is_core, peer=None):
         if peer != None:
             if msg[2] == MSG_REQUEST_FULL_CHAIN:
-                # TODO: 現状はMSG_REQUEST_FULL_CHAINの時にくらいしか想定してないけどとりあえず
                 print('Send our latest blockchain for reply to : ', peer)
-                pass
+                mychain = self.bm.get_my_blockchain()
+                chain_data = pickle.dumps(mychain, 0).decode()
+                new_message = self.cm.get_message_text(RSP_FULL_CHAIN, chain_data)
+                self.cm.send_msg(peer, new_message)
         else:
             if msg[2] == MSG_NEW_TRANSACTION:
                 # 新規transactionを登録する処理を呼び出す
@@ -110,11 +126,47 @@ class ServerCore(object):
                 else:
                     self.tp.set_new_transaction(new_transaction)
             elif msg[2] == MSG_NEW_BLOCK:
-                # TODO: 新規ブロックを検証する処理を呼び出す
-                pass
+                if not is_core:
+                    print('block received from unknown')
+                    return
+                # 新規ブロックを検証し、正当なものであればブロックチェーンに追加する
+                new_block = json.loads(msg[4])
+                print('new_block: ', new_block)
+                if self.bm.is_valid_chain(self.prev_block_hash, new_block):
+                    # ブロック生成が行われていたら、いったん停止してあげる
+                    # (threadingなのでキレイに止まらない場合あり)
+                    if self.is_bb_running:
+                        self.flag_stop_block_build = True
+                    self.prev_block_hash = self.bm.get_hash(new_block)
+                    self.bm.set_new_block(new_block)
+                    new_tp = self.bm.remove_useless_transaction(result)
+                    self.tp.renew_my_transaction(new_tp)
+                else:
+                    # ブロックとして不正ではないがVerifyにコケる婆は自分がorphanブロックを生成している可能性がある
+                    sef.get_all_chains_for_resolve_conflict()
             elif msg[2] == RSP_FULL_CHAIN:
-                # TODO: ブロックチェーン送信要求に応じて返却されたブロックチェーンを検証する処理を呼び出す
-                pass
+                # ブロックチェーン送信要求に応じて返却されたブロックチェーンを検証し、有効なものか検証した上で
+                # 自分の持つチェーンと比較し優位な方を今後のブロックチェーンとして有効化する
+
+                if not is_core:
+                    print('blockchain received from unknown')
+                    return
+                new_block_chain = pickle.loads(msg[4].encode('utf8'))
+                print(new_block_chain)
+                result, pool_4_orphan_blocks = self.bm.resolve_conflicts(new_block_chain)
+                print('blockchain received form central')
+                if result is not None:
+                    self.prev_block_hash = result
+                    if len(pool_4_orphan_blocks) != 0:
+                        # orphanブロック群の中にあった未処理扱いになる
+                        # TransactionをTransactionPoolに戻す
+                        new_transactions = self.bm.get_transactions_from_orphan_blocks(pool_4_orphan_blocks)
+
+                        for t in new_transactions:
+                            self.tp.set_new_transaction(t)
+                else:
+                    print('Received blockchain is useless...')
+
             elif msg[2] == MSG_ENHANCED:
                 # P2P Networkを単なるトランスポートして使っているアプリケーションが独自拡張したメッセージはここで処理する。SimpleBitcoinとしてはこの種別は使わない
 
