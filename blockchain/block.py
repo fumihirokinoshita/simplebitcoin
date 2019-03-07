e64e0fc1cc2e160ce2c8e463f1d6253f144b2feb
mihiro <f.mihiroxx@gmail.com>
2019-03-07 12:40:44 +0900

    complete 3.1

---
+++ blockchain/block.py
@@ -0,0 +1,32 @@
+import json
+from time import time
+
+class Block:
+    def __init__(self, transaction, previous_block_hash):
+        self.timestamp = time()
+        self.transaction = transaction
+        self.previous_block = previous_block_hash
+
+    def to_dict(self):
+        d = {
+            "timestamp" : self.timestamp,
+            "transaction": json.dumps(self.transaction),
+            "previous_block": self.previous_block,
+        }
+        return d
+
+class GenesisBlock(Block):
+    """
+    前方にブロックを持たせないブロックチェーンの始原となるブロック。
+    transactionにセットしているのは
+    「{"message":"this_is_simple_bitcoin_genesis_block"}」をSHA256でハッシュしたもの。深い意味はない
+    """
+    def __init__(self):
+        super().__init__(transaction="AD9B477B42B22CDF18B1335603D....", previous_block_hash=None)
+
+    def to_dict(self):
+        d = {
+            "transaction": self.transaction,
+            "genesis_block": True,
+        }
+        return d
